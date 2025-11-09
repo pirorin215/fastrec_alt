ef7b8ffbea36601b08ed92a7c30c9266bde5e52b
pirorin215 <hogehoge215@gmail.com>
2025-11-09 13:44:55 +0900

    Revert "hoge"
    This reverts commit c6865daf2967e6fd29e2a74a4b4134859e9d54b2.

--- lcd_display.ino
+++ lcd_display.ino
@@ -10,12 +10,6 @@ const int FONT_SIZE = 5;
 const int LINE_HEIGHT = 10;  // Assuming 10 pixels per line for font size 5
 
 void initSSD() {
-  if(g_is_ssd_initialized) {
-    return;
-  }
-    
-  applog("initSSD");
-
   Wire.begin(LCD_SDA_GPIO, LCD_SCL_GPIO);
   display.begin();
   display.setRotation(2);       // rotate 180 degrees
